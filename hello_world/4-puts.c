f7b3004de110dd6052ecd8d8f35ab2047a17904e
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-16 05:41:47 -0800

    We are the champions

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point 
+ *
+ * Return: always zero
+ */
+
 int main(void)
 {
+	char Str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Str);
+	
+	return (0);
+}
 
