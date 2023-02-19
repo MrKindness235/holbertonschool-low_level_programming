e56b9f0a1ceb302d65d5a9f58efbccadbdff0ca4
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-19 09:02:57 -0800

    Thank goodness

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - all 10 base numbers printed
+ *
+ *  return: zero
+ */
+
+int main(void)
+{
+	int n;
+	for (n = 0;n <= 9; n++)
+	printf ("%d\n",n);
+return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+We are positively coding and negatively sleeping! What a delight!
