e433406f6ac7f2fc9d2ba9bde52d15c76224f740
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-19 10:50:00 -0800

    Under the sea!

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,33 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
 /**
  * main - prints n's sign
  *
  * return: zero
  */
+
 int main(void)
 {
-		int n;
+int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
 if (n > 0)
-{ printf ("is positive") }
-else if(n = 0)
-{ printf ("is zero") } 
-else(n < 0)
-{ printf ("is negative") }
-					return (0);
+{ 
+	printf ("%d is positive\n", n);
+}
+
+else if (n == 0)
+{
+printf ("%d is zero\n", n);
+}
+
+else
+{
+printf ("%d is negative\n", n);
+}
+return (0);
 }
