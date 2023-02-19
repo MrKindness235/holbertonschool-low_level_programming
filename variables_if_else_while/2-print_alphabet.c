7072fb400334efd8bb44e844c7fb2f12d45b4f47
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-19 13:54:42 -0800

    The abcs

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- Alphabet in lowercase is displayed
+ *
+ * Return: zero
+ *
+ */
+
+int main(void)
+{
+	char ch = '0';
+	for (ch = 'a'; ch <= 'z'; ch++)
+putchar (ch);
+putchar ('\n');
+return (0);
+}
