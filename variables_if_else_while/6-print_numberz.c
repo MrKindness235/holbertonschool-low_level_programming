d5523255d34f8f6ce31149a9f838ee81b1c1ebf3
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-20 05:03:43 -0800

    High in the morining, working in coding

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main- Alphabet in lowercase is displayed
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+char n = '0';
+for (n = '0'; n <= '9'; n++)
+putchar (n);
+putchar ('\n');
+return (0);
+}
