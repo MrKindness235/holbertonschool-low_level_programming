fadac3b98943c7b5ba5e532ffd430973dff26a41
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-24 14:29:26 -0800

    And another one!

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  print_alphabet_x10 - prints alphaber 10 times
+ *
+ *  Return: zero
+ */
+
+void print_alphabet_x10(void)
+{
+	int t = 0, i;
+while (t <= 10)
+{
+	i = 97;
+	while (i <= 122)
+	{
+		_putchar (i);
+		i++;
+	}
+	t++;
+}
+	_putchar('\n');
+}
