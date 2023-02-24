3b2f5bed8eb193480883a4bf5de0e5d62361fd87
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-24 04:44:41 -0800

    Lets go!

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,15 +6,15 @@
  * Return: zero
  */
 
+int main(void)
+{
 int a[] = {95, 112, 117, 116, 99, 104, 97, 114};
 int i;
 
-int main(void)
-{
 	for (i = 0; i < 8; i++)
-       	{
- _putchar (a[i]);
-	}
-	_putchar('\n');
+{
+		_putchar (a[i]);
+}
+		_putchar('\n');
 		return (0);
 }
