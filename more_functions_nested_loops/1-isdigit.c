106514f8571e7ed8705da71ab331f3bb1cf2a511
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-24 15:05:11 -0800

    Running late! They are going to chop-off my head!

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for a digit, which is defined as a decimal between 0 and 9
+ *
+ * Return: 1 for uppercase and 'c' for not
+ * @c: an integer number
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
