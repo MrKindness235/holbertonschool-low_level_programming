df1eb13aff79161a483d35722c67ae3fd534327f
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-24 16:36:07 -0800

    Cmon! Cmon!

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,13 @@
 /**
  * _isdigit - Checks for a digit, which is defined as a decimal between 0 and 9
  *
- * Return: 1 for uppercase and 'c' for not
+ * Return: 1 for digit found and 'c' for no digit found
  * @c: an integer number
  */
 
 int _isdigit(int c)
 {
-	if (c >= 48  && c <= 57)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
