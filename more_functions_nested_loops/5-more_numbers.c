93ce6f045425b4b02a46ab7a0f8150b6b4d44714
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-24 18:03:52 -0800

    Im loosing my mind thanks to sleepness, this is fun cause it gets jankier everytime I "fix" something

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,22 @@
 
 void more_numbers(void)
 {
-int t = 0, i;
+int t = 0, i, j;
 while (t < 10)
 {
 i = 0;
-while (i <= 14)
+while (i <= 9)
 {
 _putchar (i);
 i++;
+if (i == 9)
+{
+	i = 1;
+	for (j = 0; j <= 4; j++)
+	{
+		_putchar (j);
+		i = 9;
+	}
 }
 _putchar('\n');
 t++;
