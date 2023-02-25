a5ce97470f9e5a975a78bcee7c4fae7154a9b8f2
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-25 04:51:54 -0800

    Im doing good! :]

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - Checks if 'c' is an alphabetic char
+ *
+ * Return: 1 for 'c' as an alphabetic char and 0 otherwise
+ *
+ * @c: a character
+ */
+
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
