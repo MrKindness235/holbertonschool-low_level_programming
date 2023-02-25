90d675de822849d899c43905fd340b33d39f66f6
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-24 16:46:15 -0800

    I HAVE A HAMMER AND NO MORE WALL, HAHAHAHAHAHA!

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * mul - Checks for a digit, which is defined as a decimal between 0 and 9
+ *
+ * Return: 1 for digit found and 'c' for no digit found
+ * @c: an integer number
+ */
 int mul(int a, int b)
 {
 	return (a * b);
