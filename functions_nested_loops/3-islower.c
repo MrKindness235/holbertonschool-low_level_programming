ea22b4f688f4eddcfd18273190a38b0e248bde2a
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-25 04:38:15 -0800

    Not as early in the morning as I wished, but Im still on it :]

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks if 'c' is lowercase
+ *
+ * Return: 1 for 'c' as lowercase and 0 otherwise
+ * @c: a character
+ */
+
+
+
+
+int _islower(int c);
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
