68a3e1108e454170104e5995445f3c3bd92d0b01
MrKindness235 <patriciocoitino235@gmail.com>
2023-02-28 06:43:21 -0800

    A melody is playing in my head!

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -5,18 +5,18 @@
  *
  * Return: null
  *
- * @str: Given string of chars
+ * @s: Given string of chars
  */
 
-void  _puts(char *str)
+void print_rev(char *s)
 {
 	int p = 0;
 
-	while (str[p] != '\0')
+	while (s[p] != '\0')
 	{
 		p++;
 	}
-	while (str[p] > 0)
+	while (s[p] > 0)
 	{
 		_putchar (p);
 		p--;
