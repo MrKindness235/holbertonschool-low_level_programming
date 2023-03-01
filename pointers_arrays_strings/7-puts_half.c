54f0327b8c8263945b68fa6edb1f4672d49294ef
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-01 08:10:18 -0800

    Listening to: Silent Running, Gorillaz

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ *
+ * Return: null
+ *
+ * @str: Given string of chars
+ */
+
+void puts_half(char *str)
+{
+	int p = 0;
+	int len = 0;
+	int n = 0;
+
+	len = _strlen(str);
+	str[p] = len;
+	if (len % 10 == 0)
+	{
+	while (str[p] != '\0')
+	{
+		_putchar (str[p]);
+		p++;
+	}
+	_putchar ('\n');
+	}
+	else
+	{
+		n = (len -= 1) / 2;
+		_putchar (n);
+}
+}
