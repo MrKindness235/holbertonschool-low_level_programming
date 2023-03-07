cd4006c45d1668a26d09c5a5bc481b019e7e6663
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-07 09:05:06 -0800

    Plz take me out of this KATABASIS

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,10 +1,21 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - Returns length of given string.
+ *
+ * Return: String length.
+ *
+ * @s: A string.
+ */
+
 int _strlen_recursion(char *s)
 {
+	int a = 0;
+
 	if (*s)
 	{
-	_strlen_recursion(s + 1);
+		a = (_strlen_recursion(s + 1));
+		return (a += 1);
 	}
-	return (*s);
+	return (0);
 }
