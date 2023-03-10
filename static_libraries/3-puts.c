e58da15a94a15909dd55d02bbb3c2a48053c2b11
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-10 09:47:03 -0800

    Remembered to giadd for first time in my life

--- static_libraries/3-puts.c
+++ static_libraries/3-puts.c
@@ -1,16 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: always zero
- *
- */
+ *  * _puts - Check the code.
+ *   * Description: 'Prints a string, followed by a new line, to stdout.'
+ *    * @str: is a string.
+ *     */
 
-int main(void)
+void _puts(char *str)
 {
-	char Str[] = "\"Programming is like building a multilingual puzzle";
+	int i;
 
-	puts(Str);
-	return (0);
+	for (i = 0; ; i++)
+	{
+		if (str[i] == 0)
+		{
+			break;
+		}
+		else
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
 }
