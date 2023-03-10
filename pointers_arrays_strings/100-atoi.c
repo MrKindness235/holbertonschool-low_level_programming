29ec665c2ee65dcd72b6835cf0e3e5aa561612d9
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-10 08:37:59 -0800

    I wanna sleep so badly

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,19 +13,26 @@ int _atoi(char *s)
 	int c = 0;
 	int i = 0;
 	int tof = 0;
+	int polo = 1;
 
 	while (s[c] != '\0')
 	{
+		if (s[c] == 45)
+		{
+			polo = -1;
+		}
 		while (s[c] >= 48 && s[c] <= 57)
 		{
 			i = (i * 10) + (s[c] - '0');
+			c++;
 			tof = 1;
 		}
-	if (s[c - 1] == 45)
-		{
-			i = i * -1;
-		}
-			c++;
+			if (tof == 1)
+			{
+				break;
+			}
+		c++;
 	}
+		i *= polo;
 		return (i);
 }
