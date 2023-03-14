9e437092c657ad4ade9f0e23b3130d3dbeb08b9c
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-14 05:14:14 -0700

    new code ready!

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strdup - Return a pointer to a new string which is
+ *
+ * Return: pointer.
+ *
+ * @str: given string.
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (!str)
+	{
+		return (NULL);
+	}
+	p = str;
+	return (p);
+}
