1d7c5353e318cb7f7f93bf88231c438aa47f5366
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-17 06:17:45 -0700

    Too much memory is being allocated, perhaps reducing the number of * (pointer syntax?) can be the answer. More research and tests must be performed

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	p = malloc(sizeof(int **) * height);
+	p = malloc(sizeof(int *) * height);
 	if (p == NULL)
 	{
 		return (NULL);
