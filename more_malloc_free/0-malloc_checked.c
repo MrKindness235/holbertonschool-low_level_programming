f6f1ab690f6968bd22dbf270094d0566024f85b3
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-17 08:48:00 -0700

    Newly done function. I do not think is finished yet.

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Returns pointer to newly allocated memory!
+ *
+ * Return: Pointer to newly allocated memory.
+ *
+ * @b: Lenght of said newly allocated memory.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	if (b <= 0)
+	{
+		return (0);
+	}
+	p = malloc(sizeof(unsigned int) * b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
+
