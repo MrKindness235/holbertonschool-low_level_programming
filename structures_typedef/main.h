19182e0b32f5d19780f53997d486fde7f31e2c04
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-20 09:27:31 -0700

    Im more than confused, but I thrive nonetheless!

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,23 +1,13 @@
 #ifndef _FILE_NAME_H_
 #define _FILE_NAME_H_
 
-/**
- * struct dog - Cutest animal around
- *
- * @name: Name of the doggy.
- * @age: Age of the doggo.
- * @owner: Owner of the Pupper.
- */
-
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
