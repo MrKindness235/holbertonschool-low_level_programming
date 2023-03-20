3edc88d99d6e424f05ba3ceb6f31df0e3ee2b017
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-20 09:14:48 -0700

    Sending a baseline for the "custom dog profile" task code.

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * init_dog - Initializes your own very dog profile!
+ *
+ * Return: Void.
+ *
+ * @d: Given pointer to direct initialization.
+ * @name: Given name of the pupper.
+ * @age: Given age of the cutest thing on earth.
+ * @owner: Given owner of the most lotal boi or gal.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
