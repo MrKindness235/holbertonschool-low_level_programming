89561e950763b64548c3b487964defa844429c3c
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-21 06:00:32 -0700

    A bit of confusion with duplicates and wrong case scenarios. But now, presumably, is working properly.

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,20 +14,23 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name == NULL)
+		if (d->name != NULL)
 		{
-			printf("Name: (nil)\n");
+			printf("Name: %s\n", d->name);
 		}
-		printf("Name: %s\n", d->name);
+		else
+		printf("Name: (nil)\n");
 		if (d->age < 0)
 		{
 			printf("Age: (nil)\n");
 		}
+		else
 		printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
+		if (d->owner != NULL)
 		{
-			printf("Owner: (nil)\n");
+			printf("Owner: %s\n", d->owner);
 		}
-		printf("Owner: %s\n", d->owner);
+		else
+		printf("Owner: (nil)\n");
 	}
 }
