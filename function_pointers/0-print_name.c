210423aa3ae0287d75dbb08cae9f9f1e7de7b025
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-22 09:45:17 -0700

    Cant be this simple

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_name - A bit self explanatory, isn't it?
+ *
+ * @name: Name to be printed?
+ * @f: Function.
+ *
+ * I have the small but sure feeling that, even if in concept this is not
+ * that complex (pointers but instead of pointing towards variables
+ * or idividual memory cells, they point
+ * towards entire functions.), it will be indeed,
+ * in practie, pretty much confusing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
