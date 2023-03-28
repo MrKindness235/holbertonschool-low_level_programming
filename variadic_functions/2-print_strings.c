cf2ae881d9835f89f8e33897d76b7d8b779b030d
MrKindness235 <patriciocoitino235@gmail.com>
2023-03-28 09:58:32 -0700

    Simpler than expected.

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_strings- Prints the strings.
+ *
+ * Return: void.
+ *
+ * @separator: A separator.
+ * @n: Quantity of arguments!
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list string;
+	unsigned int i;
+	const char *str;
+	char *nsep;
+
+	if (n == 0)
+	{
+		putchar('\n');
+		return;
+	}
+	if (separator == NULL)
+		nsep = "";
+	va_start(string, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(string, const char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+		if (separator == NULL && i < n - 1)
+			printf("%s", nsep);
+		else if (i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(string);
+	putchar('\n');
+}
