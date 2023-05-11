4995b2f765675b81c80e6f7f50819d1a8fb5800c
MrKindness235 <patriciocoitino235@gmail.com>
2023-05-11 05:20:15 -0700

    Seems that I have forgotten the ways of betty. >:[

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,29 +2,29 @@
 
 /**
  * add_node - Adds a new node to the chain.
- * 
+ *
  * Return: Pointer to new node.
- * 
+ *
  * @head: Head of list chain.
  * @str: Given string to be linked to the new list.
-*/
+ */
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new = malloc(sizeof(list_t));
-    
-    if (!new)
-    {
-        free (new);
-        return (0);
-    }
-    if (!head || !str)
-        return (NULL);
+list_t *new = malloc(sizeof(list_t));
+
+if (!new)
+{
+        free(new);
+return(0);
+}
+if (!head || !str)
+return (NULL);
     
     new->str = strdup(str);
     new->len = strlen(str);
     new->next = *head;
     *head = new;
 
-    return (new);
+    return(new);
 }
