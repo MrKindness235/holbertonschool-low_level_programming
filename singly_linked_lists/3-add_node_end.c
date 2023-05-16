9be8f6ef7b4b6df7c9f55ba5b5ceae6ed821cb85
MrKindness235 <patriciocoitino235@gmail.com>
2023-05-16 08:31:01 -0700

    I think the loop was broken.

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -5,32 +5,30 @@ list_t *add_node_end(list_t **head, const char *str)
     list_t *new = malloc(sizeof(list_t));
     list_t *buffer = *head;
 
-if (!new)
-{
-free(new);
-return(0);
-}
-if (!head || !str)
-return (NULL);
+    if (!new)
+    {
+        free(new);
+        return(0);
+    }
+
+    if (!head || !str)
+        return (NULL);
     
-    new->str = strdup(str);
-    if (new->str == NULL)
-    free(new-str);
+        new->str = strdup(str);
+        new->len = strlen(str);
+        new->next = NULL;
 
-    new->len = strlen(str);
-    new->next = NULL;
-
-if (head == NULL) /* if no nodes, newnode is head */
+    if (head == NULL) /* if no nodes, newnode is head */
 	{
 		*head = new;
 		return (new);
 	}
 
     while (buffer->next != NULL)
+    {
 		buffer = buffer->next;
-
-	buffer->next = new;
-
-    
+    }
+	    buffer->next = new;
+  
     return(new);
 }
